2634d3296c640fd27ee00808388b3a32cd865b26
Myroslav Semeniuk <97482927+MyrSem@users.noreply.github.com>
2022-07-17 17:56:07 +0300

    finish 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+function createGallery(array) {
+    return array.reduce(
+        (acc, item) =>
+            acc +
+            `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+          </a>`,
+        '',
+    );
+}
+
+const result = createGallery(galleryItems);
+
+const list = document.querySelector('.gallery');
+list.insertAdjacentHTML('beforeend', result);
+
+new SimpleLightbox('.gallery a', {captionsData: "alt",
+captionDelay: 250,});
 
 console.log(galleryItems);
